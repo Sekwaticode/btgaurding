c283cb6d3759e3187d7b155c789026ecb6ad42f8
Sekwaticode <lsekwati2@gmail.com>
2026-04-26 14:15:23 +0200

    Features Animations done

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import avatar1 from "@/assets/images/avatar-ashwin-santiago.jpg";
 import avatar2 from "@/assets/images/avatar-lula-meyers.jpg";
 import avatar3 from "@/assets/images/avatar-florence-shaw.jpg";
+import avatar4 from "@/assets/images/avatar-owen-garcia.jpg";
 import Avatar from "@/components/Avatar";
 import Key from "@/components/Key";
 
@@ -34,20 +35,37 @@ export default function Features() {
                     <FeatureCard
                         title="Real Time collaboration"
                         description="Work together seamlessly with conflict-free team editing"
-                        className="md:col-span-2 lg:col-span-1"
+                        className="md:col-span-2 lg:col-span-1 group"
                     >
                         <div className="aspect-video flex items-center justify-center">
                             <Avatar className="z-40">
-                                <Image src={avatar1} alt="Avatar1" className="rounded-full" />
+                                <Image
+                                    src={avatar1}
+                                    alt="Avatar1"
+                                    className="rounded-full"
+                                />
                             </Avatar>
                             <Avatar className="-ml-6 border-indigo-500 z-30">
-                                <Image src={avatar2} alt="Avatar2" className="rounded-full" />
+                                <Image
+                                    src={avatar2}
+                                    alt="Avatar2"
+                                    className="rounded-full"
+                                />
                             </Avatar>
                             <Avatar className="-ml-6 border-amber-500 z-20">
-                                <Image src={avatar3} alt="Avatar3" className="rounded-full" />
+                                <Image
+                                    src={avatar3}
+                                    alt="Avatar3"
+                                    className="rounded-full"
+                                />
                             </Avatar>
-                            <Avatar className="-ml-6 border-transparent">
-                                <div className="size-full bg-neutral-700 rounded-full inline-flex items-center justify-center gap-1">
+                            <Avatar className="-ml-6 border-transparent group-hover:border-green-500 transition">
+                                <div className="size-full bg-neutral-700 rounded-full inline-flex items-center justify-center  relative">
+                                    <Image
+                                        src={avatar4}
+                                        alt="Avatar 4"
+                                        className="absolute size-full rounded-full opacity-0 group-hover:opacity-100 transition"
+                                    ></Image>
                                     {Array.from({ length: 3 }).map((_, i) => (
                                         <span
                                             className="size-1.5 rounded-full bg-white inline-flex"
@@ -61,14 +79,22 @@ export default function Features() {
 
                     <FeatureCard
                         title="Interactive Prototyping"
-                        description="Engage your clients with prototypes user activity"
-                        className="lg:col-span-1"
+                        description="Engage your clients with prototypes that react to user activity"
+                        className="lg:col-span-1 group"
                     >
                         <div className="aspect-video flex items-center justify-center">
-                            <p className="text-4xl font-extrabold text-white/20 text-center">
-                                We've acheived{" "}
-                                <span className="bg-gradient-to-r from-purple-400 to-pink-400 bg-clip-text text-transparent">
-                                    incredible
+                            <p className="text-4xl font-extrabold text-white/20 group-hover:text-white/10 transition duration-500 text-center">
+                                We&apos;ve acheived{" "}
+                                <span className="bg-gradient-to-r from-purple-400 to-pink-400 bg-clip-text text-transparent relative">
+                                    <span>incredible</span>
+                                    <video
+                                        src="/assets/gif-incredible.mp4"
+                                        autoPlay
+                                        loop
+                                        muted
+                                        playsInline
+                                        className="absolute bottom-full left-1/2 -translate-x-1/2 rounded-2xl shadow-xl  opacity-0 group-hover:opacity-100 transition duration-500 pointer-events-none"
+                                    />
                                 </span>{" "}
                                 growth this year
                             </p>
@@ -78,12 +104,20 @@ export default function Features() {
                     <FeatureCard
                         title="Keyboard quick actions"
                         description="Powerful commands to help you create more designs quickly"
-                        className="lg:col-span-1"
+                        className="md:col-span-2 lg:col-span-1 md:col-start-2 lg:col-start-auto group"
                     >
                         <div className="aspect-video flex items-center justify-center gap-4">
-                            <Key className="w-28">shift</Key>
-                            <Key>alt</Key>
-                            <Key>C</Key>
+                          <Key className="w-28 outline outline-2 outline-offset-4 outline-transparent group-hover:outline-lime-400 transition-all duration-500 group-hover:translate-y-1">
+    shift
+</Key>
+
+<Key className="outline outline-2 outline-offset-4 outline-transparent group-hover:outline-lime-400 transition-all duration-500 group-hover:translate-y-1 delay-150">
+    alt
+</Key>
+
+<Key className="outline outline-2 outline-offset-4 outline-transparent group-hover:outline-lime-400 transition-all duration-500 group-hover:translate-y-1 delay-300">
+    C
+</Key>
                         </div>
                     </FeatureCard>
                 </div>
@@ -92,12 +126,14 @@ export default function Features() {
                     {features.map((feature) => (
                         <div
                             key={feature}
-                            className="bg-neutral-900 border border-white/10 inline-flex px-3 md:px-5 py-1.5 md:py-2 rounded-2xl gap-3 items-center"
+                            className="bg-neutral-900 border border-white/10 inline-flex px-3 md:px-5 py-1.5 md:py-2 rounded-2xl gap-3 items-center hover:scale-105 transition duration-500 group"
                         >
-                            <span className="bg-lime-400 text-neutral-950 size-5 rounded-full inline-flex justify-center text-xl">
+                            <span className="bg-lime-400 text-neutral-950 size-5 rounded-full inline-flex justify-center text-xl group-hover:rotate-45 transition duration-500">
                                 &#10038;
                             </span>
-                            <span className="font-medium md:text-lg">{feature}</span>
+                            <span className="font-medium md:text-lg">
+                                {feature}
+                            </span>
                         </div>
                     ))}
                 </div>
